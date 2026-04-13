a9d7e77148166cc2b9b8b2956fd9f12d8c1b91f2
Gleb Kornilov <esplete@gmail.com>
2026-04-13 23:14:16 +0300

    feat: add Skeleton component

---
+++ frontend/src/shared/ui/Skeleton/ui/Skeleton.tsx
@@ -0,0 +1,10 @@
+import { cn } from '@/shared/lib/utils';
+import * as React from 'react';
+
+export type SkeletonProps = React.ComponentProps<'div'>;
+
+export const Skeleton = (props: SkeletonProps) => {
+  const { className, ...rest } = props;
+
+  return <div data-slot="skeleton" className={cn('bg-muted animate-pulse rounded-md', className)} {...rest} />;
+};
