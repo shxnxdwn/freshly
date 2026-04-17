c9f8f1b6b4bc169a6248707617e9c24b341ee093
Gleb Kornilov <esplete@gmail.com>
2026-04-17 08:39:20 +0300

    feat: add label menu component

---
+++ frontend/src/shared/ui/Label/ui/Label.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import * as React from 'react';
+import { Label as LabelPrimitive } from 'radix-ui';
+import { cn } from '@/shared/lib/utils/cn';
+
+export type LabelProps = React.ComponentProps<typeof LabelPrimitive.Root>;
+
+export const Label = (props: LabelProps) => {
+  const { className, ...rest } = props;
+
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        className
+      )}
+      {...rest}
+    />
+  );
+};
