21874344162a97ea3320ae22d0bac762728343da
Gleb Kornilov <esplete@gmail.com>
2026-04-22 04:27:12 +0300

    feat: schema config

--- packages/schema/index.ts
+++ packages/schema/index.ts
@@ -3,14 +3,12 @@ import { z } from 'zod';
 
 const c = initContract();
 
-// Сущность продукта
 export const ProductSchema = z.object({
     id: z.string(),
     title: z.string().min(3),
     price: z.number().positive(),
 });
 
-// Сам контракт (все твои API эндпоинты)
 export const apiContract = c.router({
     getProducts: {
         method: 'GET',
