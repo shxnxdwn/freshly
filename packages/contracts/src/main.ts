641feba0063f60c3e7e6b6bf81bdc126ae6256d5
Gleb Kornilov <esplete@gmail.com>
2026-05-05 02:12:38 +0300

    feat: update contracts main

---
+++ packages/contracts/src/main.ts
@@ -0,0 +1,12 @@
+import { c } from './contract';
+import { userContract } from './user';
+
+export const Contract = c.router(
+    { user: userContract },
+    { pathPrefix: '/api/v1' }
+);
+
+export type TContract = typeof Contract;
+
+export * from './common';
+export * from './user';
