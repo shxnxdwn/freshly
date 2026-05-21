17fcf945898bf08a6da3f824036a2331e62c7cab
Gleb Kornilov <esplete@gmail.com>
2026-05-21 23:00:49 +0300

    feat: add intl config

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   reactCompiler: true
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin('./src/app/i18n/request.ts');
+
+export default withNextIntl(nextConfig);
