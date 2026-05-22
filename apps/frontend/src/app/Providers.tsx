6fc72cf070ec0517034d9e44b09bd5831da7d42e
Gleb Kornilov <esplete@gmail.com>
2026-05-22 12:58:04 +0300

    feat: remove sidebar provider

--- apps/frontend/src/app/Providers.tsx
+++ apps/frontend/src/app/Providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import * as React from 'react';
-import { Sidebar } from '@/shared/ui/Sidebar';
 import { Tooltip } from '@/shared/ui/Tooltip';
 import { type AbstractIntlMessages, NextIntlClientProvider } from 'next-intl';
 
@@ -16,9 +15,7 @@ export default function Providers(props: ProvidersProps) {
 
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
-      <Sidebar.Provider>
-        <Tooltip.Provider>{children}</Tooltip.Provider>
-      </Sidebar.Provider>
+      <Tooltip.Provider>{children}</Tooltip.Provider>
     </NextIntlClientProvider>
   );
 }
