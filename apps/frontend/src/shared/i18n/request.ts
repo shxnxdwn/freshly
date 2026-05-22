29d7217867b795381b466eedec07b5166384db6d
Gleb Kornilov <esplete@gmail.com>
2026-05-22 10:34:33 +0300

    feat: add config and request for i18n

---
+++ apps/frontend/src/shared/i18n/request.ts
@@ -0,0 +1,11 @@
+import { getRequestConfig } from 'next-intl/server';
+import { defaultLocale } from './config';
+
+export default getRequestConfig(async ({ requestLocale }) => {
+  const locale = (await requestLocale) ?? defaultLocale;
+
+  return {
+    locale,
+    messages: (await import(`../../messages/${locale}.json`)).default
+  };
+});
