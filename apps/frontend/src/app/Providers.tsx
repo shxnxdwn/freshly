7e1f3f838e35bcf6729878c969740718d627e5f0
Gleb Kornilov <esplete@gmail.com>
2026-05-22 10:37:17 +0300

    feat: add i18n provider

--- apps/frontend/src/app/Providers.tsx
+++ apps/frontend/src/app/Providers.tsx
@@ -3,13 +3,16 @@
 import * as React from 'react';
 import { Sidebar } from '@/shared/ui/Sidebar';
 import { Tooltip } from '@/shared/ui/Tooltip';
+import { NextIntlClientProvider } from 'next-intl';
 
 export type ProvidersProps = { children: React.ReactNode };
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <Sidebar.Provider>
-      <Tooltip.Provider>{children}</Tooltip.Provider>
-    </Sidebar.Provider>
+    <NextIntlClientProvider>
+      <Sidebar.Provider>
+        <Tooltip.Provider>{children}</Tooltip.Provider>
+      </Sidebar.Provider>
+    </NextIntlClientProvider>
   );
 }
