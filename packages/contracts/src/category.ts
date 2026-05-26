bc6016d7384c9a214a7a53ea8a141c0b27f678d2
Gleb Kornilov <esplete@gmail.com>
2026-05-26 15:43:40 +0300

    feat: remove unused fields

--- packages/contracts/src/category.ts
+++ packages/contracts/src/category.ts
@@ -5,8 +5,7 @@ import { CategoryId, SlugSchema, CommonErrors } from './common';
 export const CategorySchema = z.object({
     id: CategoryId,
     name: z.string().min(1).max(50),
-    slug: SlugSchema,
-    emoji: z.string().optional(),
+    slug: SlugSchema
 });
 
 export type TCategory = z.infer<typeof CategorySchema>;
