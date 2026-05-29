4e1a49e0c1e0fbe101d42697c05b31285ef5f688
Gleb Kornilov <esplete@gmail.com>
2026-05-29 18:01:11 +0300

    feat: update config

--- packages/db/ drizzle.config.ts
+++ packages/db/ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 import { config } from 'dotenv';
 
-config({ path: '../../.env' });
+config({ path: '.env' });
 
 export default defineConfig({
   schema: './src/schema',
