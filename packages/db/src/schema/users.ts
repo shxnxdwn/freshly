7942b510423b28acb758eae9ad0edb75552d12eb
Gleb Kornilov <esplete@gmail.com>
2026-06-02 14:43:46 +0300

    feat: update users schema

--- packages/db/src/schema/users.ts
+++ packages/db/src/schema/users.ts
@@ -1,14 +1,15 @@
-import { pgTable, text, varchar, timestamp, pgEnum, uuid } from 'drizzle-orm/pg-core';
-
-export const userRoleEnum = pgEnum('user_role', ['admin', 'user']);
+import { pgTable, text, varchar, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { userRoleEnum } from './enums';
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
   email: varchar('email', { length: 255 }).notNull().unique(),
+  passwordHash: text('password_hash').notNull(),
   name: varchar('name', { length: 20 }).notNull(),
-  avatar: text('avatar').notNull().default('cat.png'),
-  roles: userRoleEnum('roles').array().notNull().default(['user']),
-  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow()
+  avatar: varchar('avatar').notNull().default('cat.png'),
+  role: userRoleEnum('user_role').default('client').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull()
 });
 
 export type TSelectUser = typeof users.$inferSelect;
