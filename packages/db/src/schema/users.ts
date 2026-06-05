212715507ea83d7ed1fe3ae8f6d2e25ab7bde01d
Gleb Kornilov <esplete@gmail.com>
2026-06-05 20:29:28 +0300

    feat: update users schema

--- packages/db/src/schema/users.ts
+++ packages/db/src/schema/users.ts
@@ -1,16 +1,30 @@
-import { pgTable, text, varchar, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, text, varchar, timestamp, uuid, uniqueIndex } from 'drizzle-orm/pg-core';
 import { userRoleEnum } from './enums';
+import { sql } from 'drizzle-orm';
 
-export const users = pgTable('users', {
-  id: uuid('id').defaultRandom().primaryKey(),
-  email: varchar('email', { length: 255 }).notNull().unique(),
-  passwordHash: text('password_hash').notNull(),
-  name: varchar('name', { length: 20 }).notNull(),
-  avatar: varchar('avatar').notNull().default('cat.png'),
-  role: userRoleEnum('user_role').default('client').notNull(),
-  createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
-  updatedAt: timestamp('updated_at', { withTimezone: true }).defaultNow().notNull()
-});
+export const users = pgTable(
+  'users',
+  {
+    id: uuid('id').defaultRandom().primaryKey(),
+    name: varchar('name', { length: 100 }).notNull(),
+    email: varchar('email', { length: 255 }).notNull(),
+    passwordHash: text('password_hash'),
+    avatar: varchar('avatar').notNull().default('cat.png'),
+    role: userRoleEnum('user_role').default('client').notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+    updatedAt: timestamp('updated_at', { withTimezone: true })
+      .defaultNow()
+      .$onUpdate(() => sql`now()`)
+      .notNull(),
+
+    deletedAt: timestamp('deleted_at', { withTimezone: true })
+  },
+  (table) => [
+    uniqueIndex('users_email_unique_idx')
+      .on(table.email)
+      .where(sql`deleted_at IS NULL`)
+  ]
+);
 
 export type TSelectUser = typeof users.$inferSelect;
 export type TInsertUser = typeof users.$inferInsert;
