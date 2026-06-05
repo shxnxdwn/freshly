a7fee40b08b4b8ae83d08dcb4dfe9f8bec6fa6cf
Gleb Kornilov <esplete@gmail.com>
2026-06-05 20:20:23 +0300

    feat: add products schema

--- packages/db/src/schema/products.ts
+++ packages/db/src/schema/products.ts
@@ -8,7 +8,8 @@ import {
   timestamp,
   index,
   numeric,
-  check
+  check,
+  uniqueIndex
 } from 'drizzle-orm/pg-core';
 import { categories } from './categories';
 import { sql } from 'drizzle-orm';
@@ -20,8 +21,9 @@ export const products = pgTable(
     categoryId: integer('category_id')
       .references(() => categories.id, { onDelete: 'restrict' })
       .notNull(),
+    sku: varchar('sku', { length: 300 }).notNull(),
     name: varchar('name', { length: 255 }).notNull(),
-    slug: varchar('slug', { length: 255 }).notNull().unique(),
+    slug: varchar('slug', { length: 255 }).notNull(),
     description: text('description'),
     price: integer('price').notNull(),
     salePrice: integer('sale_price'),
@@ -34,10 +36,13 @@ export const products = pgTable(
     updatedAt: timestamp('updated_at', { withTimezone: true })
       .defaultNow()
       .$onUpdate(() => sql`now()`)
-      .notNull()
+      .notNull(),
+    deletedAt: timestamp('deleted_at', { withTimezone: true })
   },
   (table) => [
-    index('product_category_idx').on(table.categoryId),
-    check('min_one_image', sql`cardinality(${table.imageUrls}) >= 1`)
+    index('products_category_idx').on(table.categoryId),
+    check('min_one_image', sql`cardinality(${table.imageUrls}) >= 1`),
+    uniqueIndex('products_slug_unique_idx').on(table.slug),
+    uniqueIndex('products_sku_unique_idx').on(table.sku)
   ]
 );
