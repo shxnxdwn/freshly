13404e55a3aec06d77f471d1e2816059dd1a41cc
Gleb Kornilov <esplete@gmail.com>
2026-06-06 19:40:53 +0300

    feat: add main contract

--- packages/contracts/src/main.ts
+++ packages/contracts/src/main.ts
@@ -1,6 +1,6 @@
 import { c } from './contract';
-import { userContract } from './user';
 import { authContract } from './auth';
+import { userContract } from './user';
 import { categoryContract } from './category';
 import { productContract } from './product';
 import { reviewContract } from './review';
@@ -10,30 +10,18 @@ import { orderContract } from './order';
 import { supportContract } from './support';
 
 export const Contract = c.router(
-    {
-        user: userContract,
-        auth: authContract,
-        category: categoryContract,
-        product: productContract,
-        review: reviewContract,
-        address: addressContract,
-        cart: cartContract,
-        order: orderContract,
-        support: supportContract,
-    },
-    { pathPrefix: '/api/v1' }
+  {
+    auth: authContract,
+    user: userContract,
+    category: categoryContract,
+    product: productContract,
+    review: reviewContract,
+    address: addressContract,
+    cart: cartContract,
+    order: orderContract,
+    support: supportContract
+  },
+  { pathPrefix: '/api/v1' }
 );
 
 export type TContract = typeof Contract;
-
-export * from './common';
-export * from './address';
-export * from './auth';
-export * from './avatar';
-export * from './cart';
-export * from './category';
-export * from './order';
-export * from './product';
-export * from './review';
-export * from './support';
-export * from './user';
