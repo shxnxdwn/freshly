839a01293e76beaa1df93faa23c044ea1d965cb4
Gleb Kornilov <esplete@gmail.com>
2026-06-08 22:49:23 +0300

    feat: add index

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { env } from './config/env';
+import { buildServer } from './server';
+
+const start = async () => {
+  const app = await buildServer();
+
+  try {
+    await app.listen({ port: env.PORT, host: '0.0.0.0' });
+    console.log(`Server started on port ${env.PORT}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+void start();
